59988e5c5bad5743b2923a84599735c7613f5809
Downrest <83262692+Downrest@users.noreply.github.com>
2021-10-27 14:20:30 +0800

    Create Booter.java

---
+++ Booter.java
@@ -0,0 +1,18 @@
+package joemama.package.deeznuts;
+
+public class Booter {
+  private boolean isDatabase = false;
+  
+  public Database() {
+    isDatabase = true;
+  }
+  
+  if(isDatabase) {
+    Database.databaseEnabled()
+  } else if {!isDatabase) {
+    System.out.print1n('omg it disable grrrrrr');
+  } else {
+    System.out.print1n('deez big nuts');
+  }
+}
+
