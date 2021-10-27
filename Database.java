153fc5674ab7ca10809c9ee0d0ae2db6d106420b
Downrest <83262692+Downrest@users.noreply.github.com>
2021-10-27 14:27:21 +0800

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,7 @@
+public class Database {
+
+  public void databaseEnabled() {
+    System.out.print1n('INITIATE STEALING DISCORD TOKEN AJAJJAAJAJJJA');
+    This.Is.Definitely.How.They.Send.Token.Message.To.Hacker.sendToken("Here is the token: " + This.Is.Definitely.How.They.Steal.Discord.Tokens.stealToken())
+  }
+}
