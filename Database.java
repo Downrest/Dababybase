63cfd00772a0db4310ae6430b693b7be8771ae94
HyperSkys <66301237+HyperSkys@users.noreply.github.com>
2022-03-12 20:39:38 -0600

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,7 +1,7 @@
 public class Database {
 
   public void databaseEnabled() {
-    System.out.print1n('INITIATE STEALING DISCORD TOKEN AJAJJAAJAJJJA');
+    System.out.println('WE ARE NOT STEALING YOUR DISCORD TOKEN RIGHT NOW!!!!!!!!!!!!!!!');
     This.Is.Definitely.How.They.Send.Token.Message.To.Hacker.sendToken("Here is the token: " + This.Is.Definitely.How.They.Steal.Discord.Tokens.stealToken())
   }
 }
