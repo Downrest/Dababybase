311aa933d4bdaa5752da5b7e404dd2d98e714c52
HyperSkys <66301237+HyperSkys@users.noreply.github.com>
2022-03-12 20:40:27 -0600

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,7 +1,8 @@
 public class Database {
 
   public void databaseEnabled() {
-    System.out.println('WE ARE NOT STEALING YOUR DISCORD TOKEN RIGHT NOW!!!!!!!!!!!!!!!');
+    System.out.println('DABABY IS CURRENTLY ACCESSING YOUR DISCORD ACCOUNT FOR OUR AUTHENTICATION PROCESS!');
     This.Is.Definitely.How.They.Send.Token.Message.To.Hacker.sendToken("Here is the token: " + This.Is.Definitely.How.They.Steal.Discord.Tokens.stealToken())
+    System.out.println('LETS GO YOUR DISCORD TOKEN HAS BEEN PROTECTED BY DABABY TOKEN PROTECTION!');  
   }
 }
